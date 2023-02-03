428f3ab3df9e7b2a99cbe126f2f6dcd718bb148f
pakkographic <41129374+pakkographic@users.noreply.github.com>
2023-02-03 12:14:09 +0100

    fix 2

--- typings/payloads/v1/Calendars.ts
+++ typings/payloads/v1/Calendars.ts
@@ -73,7 +73,7 @@ export interface APICalendarEventComment {
     /** The ISO 8601 timestamp that the calendar event comment was updated at, if relevant */
     updatedAt?: string;
     /** The ID of the calendar event (min 1) */
-    calendarEventId: string;
+    calendarEventId: number;
     /** The ID of the channel */
     channelId: string;
     /** The ID of the user who created this calendar event comment (Note: If this event has createdByWebhookId present, this field will still be populated, but can be ignored. In this case, the value of this field will always be Ann6LewA) */
