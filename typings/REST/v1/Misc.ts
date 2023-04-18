500b40b97c52e8c5dfddb638eaf5fb5ef73d74de
pakkographic <41129374+pakkographic@users.noreply.github.com>
2023-04-18 19:28:40 +0200

    adding GETUserServersResponse

--- typings/REST/v1/Misc.ts
+++ typings/REST/v1/Misc.ts
@@ -1,6 +1,11 @@
-import { APIUser } from "../../payloads";
+import { APIGuild, APIUser } from "../../payloads";
 
 export interface GETUserResponse {
     /** The user to get. */
     user: APIUser
 }
+
+// no body, query GETUserServersResponse
+export interface GETUserServersResponse {
+    servers: Array<APIGuild>;
+};
