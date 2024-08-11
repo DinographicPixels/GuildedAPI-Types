9ad7e7b4184d790715e15e95927df31a2f47f2ba
pakkographic <41129374+pakkographic@users.noreply.github.com>
2024-08-11 15:30:07 +0200

    DELETEMessageReactionQuery type fix

--- typings/REST/v1/Reactions.ts
+++ typings/REST/v1/Reactions.ts
@@ -1,6 +1,6 @@
 import * as OpenAPI from "../../schemas/v1"
 
-export type DELETEMessageReactionQuery = OpenAPI.operations["ChannelMessageReactionDelete"]["parameters"]["query"];
+export type DELETEMessageReactionQuery = OpenAPI.operations["ChannelMessageReactionDeleteMany"]["parameters"]["query"];
 // /** {@link https://www.guilded.gg/docs/api/reactions/ChannelMessageReactionDeleteMany | DELETE at /channels/channelId/messages/messageId/emotes} */
 // export interface DELETEMessageReactionQuery {
 //     /** Filter out reactions to delete by this emote ID */
